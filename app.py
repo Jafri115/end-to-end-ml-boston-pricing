c4fbf846e5ce201d169f907a70379b00ee394d89
Syed Wasif Murtaza Jafri <wasifjafri94@gmail.com>
2025-01-09 00:25:58 +0100

    changes in web app and post function

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 # load model
 rfmodel = pickle.load(open('rfmodel.pkl','rb'))
-scaler = pickle.load(open('scaling.pkl','rb'))
+scaler = pickle.load(open('transformation_pipeline.pkl','rb'))
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods= ['POST'])
+@app.route('/predict_api',methods= ['POST']) # for testing api with postman
 def predict_api():
     data = request.json['data'] # fetching data key in json
     print(data)
@@ -22,6 +22,15 @@ def predict_api():
     output = rfmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods= ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()] # feticing data from html form
+    new_data = scaler.transform(np.array(data).reshape(1,-1))
+    print(new_data)
+    output = rfmodel.predict(new_data)[0]
+    print(output)
+    return render_template('home.html',prediction_text=output)
 
 if __name__=='__main__':
     app.run(debug=True)
