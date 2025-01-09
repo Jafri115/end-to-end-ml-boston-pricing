51ff401eaa4f21cd2151ac2cef8a7db7a2048ba5
Syed Wasif Murtaza Jafri <wasifjafri94@gmail.com>
2025-01-09 04:21:12 +0100

    Lr model changes back in app

--- app.py
+++ app.py
@@ -13,23 +13,11 @@ scaler = pickle.load(open('transformation_pipeline.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods= ['POST']) # for testing api with postman
-def predict_api():
-    data = request.json['data'] # fetching data key in json
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output = lrmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
 
 @app.route('/predict',methods= ['POST'])
 def predict():
-    data = [float(x) for x in request.form.values()] # feticing data from html form
-    new_data = scaler.transform(np.array(data).reshape(1,-1))
-    print(new_data)
-    output = lrmodel.predict(new_data)[0]
-    print(output)
+
+    output = 10
     return render_template('home.html',prediction_text=output)
 
 if __name__=='__main__':
